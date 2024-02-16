8ce426c0bf222fd52f7a5197f39590fe8deaad66
takato <ltakato@hotmail.com>
2024-02-16 13:53:44 -0300

    feat: listen to salesforce Account CDC

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SalesforceService } from './salesforce/salesforce.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the config globally available
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SalesforceService],
 })
 export class AppModule {}
---
+++ src/salesforce/salesforce.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+// import { InjectQueue } from '@nestjs/bull';
+// import { Queue } from 'bull';
+import * as jsforce from 'jsforce';
+
+@Injectable()
+export class SalesforceService implements OnModuleInit {
+  private conn: jsforce.Connection;
+
+  constructor(
+    private configService: ConfigService,
+    // @InjectQueue('salesforce-events') private salesforceEventsQueue: Queue,
+  ) {
+    this.conn = new jsforce.Connection({
+      loginUrl: this.configService.get('SALESFORCE_LOGIN_URL'),
+    });
+  }
+
+  async onModuleInit() {
+    await this.conn.login(
+      this.configService.get('SALESFORCE_USERNAME'),
+      this.configService.get('SALESFORCE_PASSWORD') +
+        this.configService.get('SALESFORCE_TOKEN'),
+      (err: Error) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        this.subscribeToTopic();
+      },
+    );
+  }
+
+  private subscribeToTopic() {
+    const topic = this.conn.streaming.topic('/data/AccountChangeEvent');
+    const subscription = topic.subscribe((message) => {
+      console.log('Received message from Salesforce:', message);
+      // Enqueue the message for processing
+      // this.salesforceEventsQueue.add(message);
+    });
+    console.log(
+      'Subscribed to CreatedProduct topic in Salesforce',
+      subscription._channels,
+    );
+  }
+}
